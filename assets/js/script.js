b4495f3b6b1e3b1d3bebf909b854ca9b23388bc3
Murad Jafarov <muradjj@code.edu.az>
2023-11-26 20:23:02 +0400

    design updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-function toggleDroplist() {
-    var droplistContent = document.getElementById("droplistContent");
-    droplistContent.style.display = (droplistContent.style.display === "block") ? "none" : "block";
-}
+// function toggleDroplist() {
+//     var droplistContent = document.getElementById("droplistContent");
+//     droplistContent.style.display = (droplistContent.style.display === "block") ? "none" : "block";
+// }
 
-// Stop event propagation when clicking on checkboxes or labels
-function stopPropagation(event) {
-    event.stopPropagation();
-}
+// // Stop event propagation when clicking on checkboxes or labels
+// function stopPropagation(event) {
+//     event.stopPropagation();
+// }
 
-function handleCheckboxClick(checkboxId) {
-    var checkbox = document.getElementById(checkboxId);
-    checkbox.checked = !checkbox.checked;
-}
+// function handleCheckboxClick(checkboxId) {
+//     var checkbox = document.getElementById(checkboxId);
+//     checkbox.checked = !checkbox.checked;
+// }
 
-var droplistButtons = document.getElementsByClassName("droplist-button");
+// var droplistButtons = document.getElementsByClassName("droplist-button");
 
-for (var i = 0; i < droplistButtons.length; i++) {
-    droplistButtons[i].addEventListener("click", function () {
-        var upButton = this.querySelector(".fa-chevron-up");
-        if (upButton.classList.contains("rotate")) {
-            upButton.classList.remove("rotate");
-        } else {
-            upButton.classList.add("rotate");
-        }
-    });
-}
+// for (var i = 0; i < droplistButtons.length; i++) {
+//     droplistButtons[i].addEventListener("click", function () {
+//         var upButton = this.querySelector(".fa-chevron-up");
+//         if (upButton.classList.contains("rotate")) {
+//             upButton.classList.remove("rotate");
+//         } else {
+//             upButton.classList.add("rotate");
+//         }
+//     });
+// }
